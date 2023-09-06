2fc1028607f71f61acce44b29695b021cc746de1
Petri Hänninen <hello@petrihanninen.com>
2023-09-06 15:27:54 +0300

    More git shortcuts

--- zsh/dt.zsh
+++ zsh/dt.zsh
@@ -13,3 +13,43 @@ function dt-cmd() {
   make app-run-command command="$@"
 }
 
+function get_testing_branch() {
+  repo="$(basename -s .git `git config --get remote.origin.url`)"
+  branch="main"
+
+  case "$repo" in
+    "duunitori5" )
+      branch="testing" ;;
+    "dt5admin" )
+      branch="testing" ;;
+    "jobbland-frontend" )
+      branch="development" ;;
+    "duunitori-next" )
+      branch="dev" ;;
+  esac
+
+  echo $branch
+}
+
+
+function gcos() {
+  branch="$(get_testing_branch)"
+  git checkout $branch
+}
+
+function grt() {
+  branch="$(get_testing_branch)"
+  git fetch origin $branch:$branch
+
+  hasChanges="$(git status --porcelain --untracked-files=no)"
+  if [[ $hasChanges ]]; then
+    git stash
+  fi
+
+  git rebase $branch
+  
+  if [[ $hasChanges ]]; then
+    git stash pop
+  fi
+}
+
