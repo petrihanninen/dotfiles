8021cf8e862f625ac613106f832e51b736e64a06
Petri Hänninen <hello@petrihanninen.com>
2024-08-08 11:38:54 +0300

    Better vim scrolling

--- config/.config/nvim/lua/config/keymaps.lua
+++ config/.config/nvim/lua/config/keymaps.lua
@@ -1,12 +1,15 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local map = vim.api.nvim_set_keymap
 
 -- increment & decrement
-local map = vim.api.nvim_set_keymap
 map("n", "<M-=>", "<C-a>", {})
 map("n", "<M-->", "<C-x>", {})
 
 -- Scroll down and up keeping the cursor in the same position
-map("n", "<C-d>", "<C-d>zz", {})
+map("n", "<C-b>", "<C-b>zz", {})
+map("n", "<C-f>", "<C-f>zz", {})
 map("n", "<C-u>", "<C-u>zz", {})
+map("n", "<C-d>", "<C-d>zz", {})
+
