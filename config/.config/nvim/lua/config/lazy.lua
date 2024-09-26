289415a340ba9b70cf14d987a5875bb7e5a4bc45
Petri Hänninen <hello@petrihanninen.com>
2024-09-26 11:54:53 +0300

    More nvim config

--- config/.config/nvim/lua/config/lazy.lua
+++ config/.config/nvim/lua/config/lazy.lua
@@ -6,6 +6,8 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
+vim.opt.swapfile = false
+
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
--- config/.config/nvim/lua/plugins/telescope.lua
+++ config/.config/nvim/lua/plugins/telescope.lua
@@ -1,5 +1,22 @@
 return {
   "nvim-telescope/telescope.nvim",
+  dependencies = {
+    {
+      "nvim-telescope/telescope-live-grep-args.nvim",
+      version = "^1.0.0",
+    },
+  },
+  config = function()
+    local telescope = require("telescope")
+
+    -- first setup telescope
+    telescope.setup({
+      -- your config
+    })
+
+    -- then load the extension
+    telescope.load_extension("live_grep_args")
+  end,
   keys = {
     {
       "<leader><space>",
@@ -7,6 +24,11 @@ return {
         require("telescope.builtin").find_files()
       end,
       desc = "Find files",
+    },
+    {
+      "<leader>sx",
+      ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>",
+      desc = "Find files (ripgrep)",
     },
   },
 }
