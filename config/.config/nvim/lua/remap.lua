760a1adfa6da2e8c87a61cc2373be4c9b7515211
Petri Hänninen <hello@petrihanninen.com>
2024-10-19 21:49:42 +0300

    Remove existing lsp config

--- config/.config/nvim/lua/remap.lua
+++ config/.config/nvim/lua/remap.lua
@@ -98,56 +98,6 @@ vim.keymap.set("n", "\\", "<cmd>cnext<CR>zz", { desc = "Prev Quickfix list item"
 vim.keymap.set("n", "<leader>qq", "<cmd>cclose<CR>", { desc = "Quickfix Quit" })
 
 
-autocmd("LspAttach", {
-  group = p3AutoGroup,
-  callback = function(e)
-    local opts = { buffer = e.buf }
-
-    -- Code Symbols
-    vim.keymap.set("n", "gd", function()
-      vim.lsp.buf.definition()
-    end, opts, { desc = "Go to Definition" })
-    vim.keymap.set("n", "gD", function()
-      vim.lsp.buf.type_definition()
-    end, opts, { desc = "Go to Definition" })
-    vim.keymap.set("n", "gr", function()
-      vim.lsp.buf.references()
-    end, opts, { desc = "Go to References" })
-    vim.keymap.set("n", "K", function()
-      vim.lsp.buf.hover()
-    end, opts, { desc = "Hover" })
-
-    -- Diagnostics
-    vim.keymap.set("n", "[d", function()
-      vim.diagnostic.goto_next()
-    end, opts, { desc = "Next Diagnostics" })
-    vim.keymap.set("n", "]d", function()
-      vim.diagnostic.goto_prev()
-    end, opts, { desc = "Prev Diagnostics" })
-
-    -- Code actions
-    vim.keymap.set({ "n", "v" }, "<leader>ca", function()
-      vim.lsp.buf.code_action()
-    end, opts, { desc = "Code Actions" })
-    vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format, { desc = "Code Format" })
-    vim.keymap.set("n", "<leader>cs", builtin.lsp_document_symbols, { desc = "Code Symbols" })
-
-    -- Quickfix list
-    vim.keymap.set("n", "[q", "<cmd>cprev<CR>zz", { desc = "Next Quickfix list item" })
-    vim.keymap.set("n", "]q", "<cmd>cnext<CR>zz", { desc = "Prev Quickfix list item" })
-    vim.keymap.set("n", "<leader>qq", "<cmd>cclose<CR>", { desc = "Quickfix Quit" })
-
-    -- Misc
-    vim.keymap.set("n", "<leader>cm", "<cmd>Mason<CR>", { desc = "Code Mason: open" })
-    vim.keymap.set("n", "<leader>ci", "<cmd>LspInfo<CR>", { desc = "Code Info (LSP)" })
-
-    -- Some keymaps from primeagen to check out
-    -- vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts, { desc = "" })
-    -- vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts, { desc = "" })
-    -- vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts, { desc = "" })
-    -- vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts, { desc = "" })
-  end,
-})
 
 autocmd("filetype", {
   group = p3AutoGroup,
