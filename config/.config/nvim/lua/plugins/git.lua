60d1403bf330a5433fd07f0ab90c6f4a7e3c9141
Petri Hänninen <hello@petrihanninen.com>
2024-10-21 13:14:52 +0300

    Format git.lua

--- config/.config/nvim/lua/plugins/git.lua
+++ config/.config/nvim/lua/plugins/git.lua
@@ -1,11 +1,11 @@
 return {
-  {
-    "zivyangll/git-blame.vim",
-    config = function()
-      vim.keymap.set("n", "<Leader>gp", ":<C-u>call gitblame#echo()<CR>")
-    end,
-  },
-  {
-    "airblade/vim-gitgutter",
-  },
+--   {
+--     "zivyangll/git-blame.vim",
+--     config = function()
+--       vim.keymap.set("n", "<Leader>gp", ":<C-u>call gitblame#echo()<CR>")
+--     end,
+--   },
+--   {
+--     "airblade/vim-gitgutter",
+--   },
 }
