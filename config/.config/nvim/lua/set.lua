e56e6cf32c37f98dd25210d071bd59b98eb16fe5
Petri Hänninen <hello@petrihanninen.com>
2024-10-30 08:31:06 +0200

    Basic settings sorting etc

--- config/.config/nvim/lua/set.lua
+++ config/.config/nvim/lua/set.lua
@@ -1,8 +1,11 @@
-vim.opt.number = true         -- Show line numbers
-vim.opt.relativenumber = true -- Show relative line numbers
+-- Left side
+vim.opt.number = true
+vim.opt.relativenumber = true
 vim.opt.numberwidth = 2
-vim.opt.history = 500         -- Remember n lines in history
-vim.opt.colorcolumn = "100"   -- Show a column at 100 characters
+vim.opt.signcolumn = "yes:2"
+
+-- Right side
+vim.opt.colorcolumn = "120"
 
 -- Indentation
 vim.opt.tabstop = 2
@@ -10,32 +13,39 @@ vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 
-vim.opt.ignorecase = true -- Ignore case when searching
-vim.opt.smartcase = true  -- Override the 'ignorecase' option if the search pattern contains upper case characters
+-- Search
+-- Ignore case unless there are caps in search
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+-- Highlight search results when typing
 vim.opt.hlsearch = true
 vim.opt.incsearch = true
 
-vim.opt.wildmode = "list:longest,list:full" -- Command-line completion mode
-
-vim.opt.list = false                        -- Show invisible characters
-
-vim.opt.conceallevel = 0                    -- Show concealed text
-
-vim.opt.swapfile = false                    -- Don't create swap files
-
-vim.opt.spell = false                       -- Don't spell check
-
-vim.opt.termguicolors = true                -- Enable 24-bit RGB color
-vim.opt.updatetime = 100                    -- Faster completion
-
-vim.opt.scrolloff = 8                       -- Keep no lines above and below the cursor when scrolling
-vim.opt.cursorline = true
-
+-- Swap & undo stuff
+vim.opt.swapfile = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
+-- Netrw
 vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
 vim.g.netrw_bufsettings = "noma nomod nonu nobl nowrap ro rnu"
 
-vim.opt.signcolumn = "yes:2"
+-- Splits
+vim.opt.splitright = true
+vim.opt.splitbelow = false
+
+-- Tab completion
+vim.opt.wildmode = "list:longest"
+
+-- Invisible characters
+vim.opt.list = true
+vim.opt.listchars = "leadmultispace:| ,tab:| "
+
+-- Misc
+vim.opt.history = 1000
+vim.opt.spell = false
+vim.opt.termguicolors = true
+vim.opt.updatetime = 100
+vim.opt.scrolloff = 8
+vim.opt.cursorline = true
