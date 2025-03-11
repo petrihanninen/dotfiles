7b2fd10f8ec8e5367a416fd21c02f719e3aeb887
Petri Hänninen <hello@petrihanninen.com>
2025-03-11 19:12:28 +0200

    Update tools

--- setup_new_mac/tools.sh
+++ setup_new_mac/tools.sh
@@ -9,7 +9,7 @@ echo "Installing homebrew"
 (
   echo
   echo 'eval "$(/opt/homebrew/bin/brew shellenv)"'
-) >>/Users/petrihanninen/.zprofile
+) >>"$HOME/.zprofile"
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
 # Git
@@ -18,11 +18,11 @@ brew install gh
 # CLI  tools
 brew install ripgrep
 brew install fd
-brew install trash
 brew install wget
 brew install jq
 brew install gnu-sed
 brew install gpg
+brew install jesseduffield/lazygit/lazygit
 
 # Fancy CLI stuff
 brew install --cask alacritty
@@ -30,25 +30,17 @@ brew install tmux
 brew install neovim
 brew install go php rust lua luarocks
 
-# Devving
-brew install colima docker docker-compose docker-buildx direnv kubectl
-brew install awscli
-brew install pnpm 
-brew install --cask runjs
-
-# NVM
+# Node
 brew install nvm
-nvm install 20
+nvm install --lts
+brew install pnpm
+
+# Devving
+brew install colima docker docker-compose docker-buildx direnv
+brew install kubectl
+brew install awscli
 
 # Other Apps
-brew install --cask clipy
-brew install --cask figma
-brew install --cask itsycal
-brew install --cask jukebox
-brew install --cask rectangle
-brew install --cask spotify
-brew install --cask tomatobar
-brew install --cask visual-studio-code
-brew install --cask zoom
+brew install spotify
 brew install raycast
 
