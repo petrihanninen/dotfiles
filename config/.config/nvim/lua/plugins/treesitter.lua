f1867f4d3905526dfc9cd4703388900614c516a5
Petri Hänninen <hello@petrihanninen.com>
2026-05-25 14:01:25 +0300

    Update treesitter config

--- config/.config/nvim/lua/plugins/treesitter.lua
+++ config/.config/nvim/lua/plugins/treesitter.lua
@@ -1,12 +1,25 @@
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
+		branch = "main",
 		build = ":TSUpdate",
+		lazy = false,
 		config = function()
-			require("nvim-treesitter.configs").setup({
-				auto_install = true,
-				highlight = { enable = true },
-				indent = { enable = true },
+			require("nvim-treesitter").setup({})
+
+
+			vim.api.nvim_create_autocmd("FileType", {
+				group = P3AutoGroup,
+				callback = function(ev)
+					local lang = vim.treesitter.language.get_lang(vim.bo[ev.buf].filetype)
+					if not lang then
+						return
+					end
+					if not pcall(vim.treesitter.language.add, lang) then
+						return
+					end
+					pcall(vim.treesitter.start, ev.buf, lang)
+				end,
 			})
 		end,
 	},
